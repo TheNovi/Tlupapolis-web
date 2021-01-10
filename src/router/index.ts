d07adff39bd03cf281ae0192d87643e394e4aeb2
TheNovi <jakub.novi.novacek@gmail.com>
2021-01-10 22:44:04 +0100

    Readded rest of views

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "@/views/Home.vue";
 import Rules from "@/views/Rules.vue";
+import Join from "@/views/Join.vue";
+import Contact from "@/views/Contact.vue";
 import Calendar from "@/views/Calendar.vue";
 
 Vue.use(VueRouter);
@@ -10,16 +12,31 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "Home",
+    meta: {},
     component: Home
   },
   {
     path: "/rules",
     name: "Rules",
+    meta: { title: "Pravidla" },
     component: Rules
+  },
+  {
+    path: "/join",
+    name: "Join",
+    meta: { title: "Jak se Připojit" },
+    component: Join
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    meta: { title: "Kontakt" },
+    component: Contact
   },
   {
     path: "/calendar",
     name: "Calendar",
+    meta: { title: "Kalendář" },
     component: Calendar
   }
 ];
