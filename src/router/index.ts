693c728434c9f30490c11c74ba44b6deed05c697
TheNovi <jakub.novi.novacek@gmail.com>
2021-01-10 22:48:03 +0100

    Inner auto view import

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,45 +1,61 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "@/views/Home.vue";
-import Rules from "@/views/Rules.vue";
-import Join from "@/views/Join.vue";
-import Contact from "@/views/Contact.vue";
-import Calendar from "@/views/Calendar.vue";
+// import Home from "@/views/Home.vue";
+// import Rules from "@/views/Rules.vue";
+// import Join from "@/views/Join.vue";
+// import Contact from "@/views/Contact.vue";
+// import Calendar from "@/views/Calendar.vue";
 
 Vue.use(VueRouter);
 
-const routes: Array<RouteConfig> = [
-  {
-    path: "/",
-    name: "Home",
-    meta: {},
-    component: Home
-  },
-  {
-    path: "/rules",
-    name: "Rules",
-    meta: { title: "Pravidla" },
-    component: Rules
-  },
-  {
-    path: "/join",
-    name: "Join",
-    meta: { title: "Jak se Připojit" },
-    component: Join
-  },
-  {
-    path: "/contact",
-    name: "Contact",
-    meta: { title: "Kontakt" },
-    component: Contact
-  },
-  {
-    path: "/calendar",
-    name: "Calendar",
-    meta: { title: "Kalendář" },
-    component: Calendar
-  }
-];
+// const routes: Array<RouteConfig> = [
+//   {
+//     path: "/",
+//     name: "Home",
+//     meta: {},
+//     component: Home
+//   },
+//   {
+//     path: "/rules",
+//     name: "Rules",
+//     meta: { title: "Pravidla" },
+//     component: Rules
+//   },
+//   {
+//     path: "/join",
+//     name: "Join",
+//     meta: { title: "Jak se Připojit" },
+//     component: Join
+//   },
+//   {
+//     path: "/contact",
+//     name: "Contact",
+//     meta: { title: "Kontakt" },
+//     component: Contact
+//   },
+//   {
+//     path: "/calendar",
+//     name: "Calendar",
+//     meta: { title: "Kalendář" },
+//     component: Calendar
+//   }
+// ];
+
+const routes: Array<RouteConfig> = [];
+
+const requireModule = require.context("@/views", false, /\.vue$/);
+
+requireModule.keys().forEach(filename => {
+  const c = requireModule(filename).default || requireModule(filename);
+  routes.push({
+    path: "/" + c.name.toLowerCase().replace(/^home$/, ""),
+    name: c.name,
+    component: c,
+    meta: {
+      title: c.title
+    }
+  });
+});
 
 const router = new VueRouter({
   mode: "history",
