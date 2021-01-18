a21797dec0e2bcd9b95518cc6b0299607d4ef69f
TheNovi <jakub.novi.novacek@gmail.com>
2021-01-18 22:30:00 +0100

    Fix purgecss (I hope atleast)

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,25 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 // const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+const glob = require("glob");
+
 module.exports = {
-  // configureWebpack: {plugins: [new BundleAnalyzerPlugin()]},
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  configureWebpack: {
+    plugins: [
+      // new BundleAnalyzerPlugin(),
+      new PurgecssPlugin({
+        paths: glob
+          .sync(`${__dirname}/node_modules/vuetify/**/*.ts`)
+          .concat(glob.sync(`${__dirname}/src/**/*`, { nodir: true })),
+        safelist: [
+          "v-application",
+          "v-application--wrap",
+          /^v-((?!application).)*$/,
+          /^theme--*/,
+          /.*-transition/
+        ]
+      })
+    ]
+  }
 };
