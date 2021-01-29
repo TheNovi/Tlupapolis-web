7530aeaf691a445fe81a3ec3a14484d06ef92489
TheNovi <jakub.novi.novacek@gmail.com>
2021-01-29 23:52:06 +0100

    Basic rules view fun

--- src/views/Rules.vue
+++ src/views/Rules.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
-    Tady budou pravidla.
+    <div v-for="(section, i) in rules" :key="i">
+      {{ section.name }}
+      <v-expansion-panels focusable>
+        <v-expansion-panel
+          v-for="(paragraph, i) in section.paragraphs"
+          :key="i"
+        >
+          <v-expansion-panel-header>
+            {{ paragraph.name }}
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <div v-for="(point, i) in paragraph.points" :key="i">
+              {{ i + 1 }}. {{ point }}
+            </div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { namespace } from "vuex-class";
+import { Section } from "@/model/Rules";
+const rules = namespace("rules");
 
 @Component
-export default class Rules extends Vue {}
+export default class Rules extends Vue {
+  @rules.State
+  rules!: Array<Section>;
+}
 </script>
