1be1ba7018db7f95ae58ea11518a3e024d7d82b8
TheNovi <jakub.novi.novacek@gmail.com>
2021-02-02 00:22:33 +0100

    Basic Join view

--- src/views/Join.vue
+++ src/views/Join.vue
@@ -1,6 +1,34 @@
 <template>
   <div>
-    Jak se připojit? No to bych také rád věděl.
+    <ol>
+      <li>
+        <a
+          href="https://docs.google.com/document/d/1miIKm0K7b8-fOGfOSPUOzYUsiUs2U16xNGB7XTNsBvc
+"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Zde</a
+        >
+        je dukument jak vše nastavit
+      </li>
+      <li>
+        Přijď na
+        <a
+          href="https://discord.gg/5M2wPdw
+"
+          target="_blank"
+          rel="noopener noreferrer"
+          >DeeThane discord
+        </a>
+      </li>
+      <li>
+        Podívej se do minecraft roomky komu máš napsat ohledně náboru.
+        (Momentálně to je Majkle)
+      </li>
+      <li>Slušně, pomocí soukromé zprávy, popros daného člověka o nábor.</li>
+      <li>Projdi náborem</li>
+      <li>Připoj se na server</li>
+    </ol>
   </div>
 </template>
 
